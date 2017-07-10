6f5294a75a9b5c88901c83f338699bf1637fea92
Kamil Chmielewski <kamil.chm@gmail.com>
2017-07-10 19:48:55 +0200

    vim-go: more highliting options

--- vim-config/default.nix
+++ vim-config/default.nix
@@ -45,6 +45,15 @@ in with pkgs; vim_configurable.customize {
       " Enable integration with airline.
       let g:airline#extensions#ale#enabled = 1
 
+      let g:go_highlight_build_constraints = 1
+      let g:go_highlight_extra_types = 1
+      let g:go_highlight_fields = 1
+      let g:go_highlight_functions = 1
+      let g:go_highlight_methods = 1
+      let g:go_highlight_operators = 1
+      let g:go_highlight_structs = 1
+      let g:go_highlight_types = 1
+
       let g:go_fmt_command = "goimports"
 
       autocmd FileType go nmap <leader>b  <Plug>(go-build)
