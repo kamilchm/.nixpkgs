f7703c52970b2b98f92c6aae1c9bd8ff26bb35aa
Kamil Chmielewski <kamil.chm@gmail.com>
2019-04-01 20:19:30 +0200

    nxs: simplest possible OCaml shell

--- bash-config/shells/ocaml.nix
+++ bash-config/shells/ocaml.nix
@@ -3,14 +3,12 @@ with import <nixpkgs> {};
 mkShell {
   buildInputs = with ocamlPackages_latest;
                   [ opam ocaml autoconf automake gnum4
-                  ncurses pkgconfig gmp zlib which ];
+                  ncurses pkgconfig gmp zlib which perl ];
 
   shellHook = ''
     if [ ! -d _opam ]; then
-      opam switch create ./ ocaml-system --deps-only
+      opam switch create . ocaml-system --deps-only
     fi
     eval $(opam env)
-
-    alias setup-dev="opam install utop merlin ocp-indent dune"
   '';
 }
