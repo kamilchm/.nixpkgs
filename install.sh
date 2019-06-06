20dd8644e6950452ea66bf1c84d6db05730f3669
Kamil Chmielewski <kamil.chm@gmail.com>
2019-06-06 10:43:28 +0200

    Don't remove all ~/.config, replace only managed cofigs

--- install.sh
+++ install.sh
@@ -8,7 +8,13 @@ pushd $HOME > /dev/null
 
 echo "Creating dotfiles links in user home"
 find .nix-profile/userHome/ -maxdepth 1 | sed "s/.nix-profile\/userHome\///g" | \
+    grep -v "^.config$" | \
     grep -v "^$" | xargs -I {} sh -c 'rm -rf {} && ln -sf .nix-profile/userHome/{} {}'
+
+echo "Creating .config links in user .config"
+find .nix-profile/userHome/.config -maxdepth 1 | sed "s/.nix-profile\/userHome\/.config\///g" | \
+    grep -v "^.nix-profile/userHome/.config$" | \
+    grep -v "^$" | xargs -I {} sh -c 'rm -rf .config/{} && ln -sf ../.nix-profile/userHome/.config/{} .config/{}'
 
 popd > /dev/null
 
