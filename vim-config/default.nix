c7c7714d0d574fc1d5ad59f68d4aff44d19e5bd8
Kamil Chmielewski <kamil.chm@gmail.com>
2019-07-22 14:09:17 +0200

    vim: use javascript language server from npx

--- vim-config/default.nix
+++ vim-config/default.nix
@@ -74,6 +74,7 @@ in with pkgs; neovim.override {
           \ 'reason': ['ocaml-language-server', '--stdio'],
           \ 'purescript': ['npx', 'purescript-language-server', '--stdio'],
           \ 'typescript': ['npx', 'typescript-language-server', '--stdio'],
+          \ 'javascript': ['npx', '-p', 'javascript-typescript-langserver', 'javascript-typescript-stdio'],
           \ }
       " Automatically start language servers.
       let g:LanguageClient_autoStart = 1
