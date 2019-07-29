7cec6947c698089c611e6c2322ba8a00ded45742
Kamil Chmielewski <kamil.chm@gmail.com>
2019-07-29 14:45:52 +0200

    Refactor installing dotfiles -> link files only

--- install.sh
+++ install.sh
@@ -4,17 +4,10 @@ set -e
 echo "Installing nix profile"
 nix-env -i all
 
-pushd $HOME > /dev/null
+pushd $HOME/.nix-profile/userHome > /dev/null
 
-echo "Creating dotfiles links in user home"
-find .nix-profile/userHome/ -maxdepth 1 | sed "s/.nix-profile\/userHome\///g" | \
-    grep -v "^.config$" | \
-    grep -v "^$" | xargs -I {} sh -c 'rm -rf {} && ln -sf .nix-profile/userHome/{} {}'
-
-echo "Creating .config links in user .config"
-find .nix-profile/userHome/.config -maxdepth 1 | sed "s/.nix-profile\/userHome\/.config\///g" | \
-    grep -v "^.nix-profile/userHome/.config$" | \
-    grep -v "^$" | xargs -I {} sh -c 'rm -rf .config/{} && ln -sf ../.nix-profile/userHome/.config/{} .config/{}'
+echo "Creating dotfiles links"
+find . -follow -type f -printf "rm -rf '$HOME/%p' && mkdir -p '$HOME/%h' && ln -s '$HOME/.nix-profile/userHome/%p' '$HOME/%p'\n" | xargs -I {} sh -c {}
 
 popd > /dev/null
 
