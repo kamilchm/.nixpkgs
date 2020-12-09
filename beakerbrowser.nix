b711da5b943f905d3711d572688fd87f85676015
Kamil Chmielewski <kamil.chm@gmail.com>
2020-12-09 02:26:34 +0100

    beakerbrowser: 0.8.8 -> 1.1.0

--- beakerbrowser.nix
+++ beakerbrowser.nix
@@ -2,7 +2,7 @@
 appimageTools.wrapType2 {
   name = "beakerbrowser";
   src = fetchurl {
-    url = "https://github.com/beakerbrowser/beaker/releases/download/0.8.8/Beaker.Browser.0.8.8.AppImage";
-    sha256 = "15fnxam41kkggiws4ci7r8k4gca27rx9ankblxicpck66b5g3451";
+    url = "https://github.com/beakerbrowser/beaker/releases/download/1.1.0/Beaker.Browser-1.1.0.AppImage";
+    sha256 = "07hcyr6vyim8vrvw120v5jjfvy5jkcaqc3wgqgyf6prbqdx71dkp";
   };
 }
