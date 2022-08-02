f9e6d93516b13ed9e9c8787d26ea0f554f0d4d8e
Kamil Chmielewski <kamil.chm@gmail.com>
2022-08-02 03:30:47 +0200

    vim: init.vim -> init.lua + Neovim LSP

--- vim-config/default.nix
+++ vim-config/default.nix
@@ -7,177 +7,52 @@ in with pkgs; neovim.override {
   vimAlias = true;
   configure = {
     customRC = ''
-      syntax on
-      filetype on
-      set expandtab
-      set tabstop=2
-      set softtabstop=0
-      set shiftwidth=2
-      set smarttab
-      set autoindent
-      set smartindent
-      set smartcase
-      set ignorecase
-      set modeline
-      set nocompatible
-      set encoding=utf-8
-      set incsearch
-      set hlsearch
-      set history=700
-      set number
-      set laststatus=2
-      set signcolumn=yes
-
-      set termguicolors
-      set background=dark
-      colorscheme molokai
-      let g:airline_theme = 'molokai'
-
-      set grepprg=rg\ --
-      nnoremap <silent> F :Rg<CR>
-
-      " let g:ctrlp_user_command = 'rg --files %s'
-      let g:ctrlp_user_command = 'fd -H --type f --color=never "" %s'
-      let g:ctrlp_use_caching = 0
-
-      let g:fruzzy#usenative = 1
-      let g:ctrlp_match_func = {'match': 'fruzzy#ctrlp#matcher'}
-
-      " autocompletion
-      let g:deoplete#enable_at_startup = 1
-      " use CTRL+Space to autocomplete
-      inoremap <C-Space> <C-x><C-o>
-      inoremap <C-@> <C-Space>
-
-      let delimitMate_expand_cr = 1
-      let delimitMate_expand_space = 1
-
-      " Error and warning signs.
-      let g:ale_sign_error = '⤫'
-      let g:ale_sign_warning = '⚠'
-
-      " Enable integration with airline.
-      let g:airline#extensions#ale#enabled = 1
-
-      " disbale deno as a linter
-      let g:ale_linters_ignore = {
-        \ 'typescript': ['deno'],
-        \ 'typescriptreact': ['deno'],
-        \ }
-
-      let g:rooter_patterns = ['package.json', '.git', '.git/', '_darcs/', '.hg/', '.bzr/', '.svn/']
-
-      let g:go_highlight_build_constraints = 1
-      let g:go_highlight_extra_types = 1
-      let g:go_highlight_fields = 1
-      let g:go_highlight_functions = 1
-      let g:go_highlight_methods = 1
-      let g:go_highlight_operators = 1
-      let g:go_highlight_structs = 1
-      let g:go_highlight_types = 1
-
-      " let g:go_fmt_command = "goimports"
-
-      autocmd FileType go nmap <leader>b  <Plug>(go-build)
-      autocmd FileType go nmap <leader>r  <Plug>(go-run)
-      autocmd FileType go nmap <leader>t  <Plug>(go-test)
-      autocmd FileType go nmap <Leader>c  <Plug>(go-coverage-toggle)
-
-      autocmd FileType dhall nnoremap T :!dhall-to-json --file %<CR>
-      autocmd FileType dhall nnoremap Y :!dhall-to-yaml --file %<CR>
-
-      let g:LanguageClient_serverCommands = {
-          \ 'nim': ['nimlangserver'],
-          \ 'python': ['pyls'],
-          \ 'haskell': ['hie-wrapper'],
-          \ 'rust': ['rls'],
-          \ 'zig': ['zls'],
-          \ 'ocaml': ['ocamllsp'],
-          \ 'purescript': ['pnpm', 'dlx', 'purescript-language-server', '--stdio'],
-          \ 'dhall': ['dhall-lsp-server'],
-          \ 'javascript': ['pnpm', 'dlx', 'typescript-language-server', '--stdio'],
-          \ 'typescript': ['pnpm', 'dlx', 'typescript-language-server', '--stdio'],
-          \ 'typescriptreact': ['pnpm', 'dlx', 'typescript-language-server', '--stdio'],
-          \ }
-      " Automatically start language servers.
-      let g:LanguageClient_autoStart = 1
-      let g:LanguageClient_signColumnAlwaysOn = 1
-      let g:LanguageClient_loggingFile = expand('~/.vim/LanguageClient.log')
-      " let g:LanguageClient_loggingLevel = 'DEBUG'
-
-      let g:neoformat_javascript_prettier = {
-            \ 'exe': 'pnpx',
-            \ 'args': ['-y', 'prettier'],
-            \ 'valid_exit_codes': [0],
-            \ }
-
-      let g:neoformat_enabled_javascript = ['prettier']
-
-      au BufEnter *.ml setf ocaml
-      au BufEnter *.mli setf ocaml
-      au BufEnter *.atd setf ocaml
-      au FileType ocaml set shiftwidth=2 tabstop=2
-
-      " let g:opamshare = substitute(system('${pkgs.opam}/bin/opam config var share'),'\n$',''','''''')
-      " execute "set rtp+=" . g:opamshare . "/merlin/vim"
-
-      au FileType markdown setlocal spell
-
-      nnoremap <silent> K :call LanguageClient_textDocument_hover()<CR>
-      " nnoremap <silent> <cr> :call LanguageClient_textDocument_hover()<cr>
-      nnoremap <silent> E :call LanguageClient#explainErrorAtPoint()<CR>
-      nnoremap <silent> <F2> :call LanguageClient_textDocument_rename()<CR>
-      nnoremap <silent> gd :call LanguageClient_textDocument_definition()<cr>
-      nnoremap <silent> gf :call LanguageClient_textDocument_formatting()<cr>
-
-      nnoremap <silent> T :TestNearest<CR>
+      luafile ${./init.lua}
     '';
 
-    vam.knownPlugins = vimPlugins // my_plugins // { "tlib" = vimPlugins.tlib_vim; };
-    vam.pluginDictionaries = [
-      { names = [
-        "ale"
-        "ctrlp"
-        "fruzzy"
-        "vim-rooter"
-        "vim-nix"
-        "fzfWrapper"
-        "vim-ripgrep"
-        "deoplete-nvim"
-        "LanguageClient-neovim"
-        "editorconfig-vim"
-        "neoformat"
-        "delimitMate"
-        "tcomment_vim"
-        "vim-test"
-        "molokai"
-        "fugitive"
-        "gitgutter"
-        "git-messenger-vim"
-        "vim-airline"
-        "vim-airline-themes"
-        "sleuth"
-        "rust-vim"
-        "zig-vim"
-        "vim-go"
-        "vim-javascript"
-        "vim-jsx-pretty"
-        "yats-vim"
-        "vim-graphql"
-        "vim-vue"
-        "elm-vim"
-        "vim-pony"
-        "nim-vim"
-        "vim-elixir"
-        "alchemist-vim"
-        "vim-ocaml"
-        "purescript-vim"
-        "haskell-vim"
-        "dhall-vim"
-        "vim-toml"
-        "vim-pug"
-        "hexmode"
-      ]; }
+    packages.all.start = with pkgs.vimPlugins; with my_plugins; [
+      ale
+      ctrlp
+      fruzzy
+      vim-rooter
+      vim-nix
+      fzfWrapper
+      vim-ripgrep
+      editorconfig-vim
+      nvim-lspconfig
+      nvim-cmp
+      cmp-nvim-lsp
+      cmp_luasnip
+      luasnip
+      delimitMate
+      tcomment_vim
+      molokai
+      fugitive
+      gitgutter
+      git-messenger-vim
+      vim-airline
+      vim-airline-themes
+      sleuth
+      rust-vim
+      zig-vim
+      vim-go
+      vim-javascript
+      vim-jsx-pretty
+      yats-vim
+      vim-graphql
+      vim-vue
+      elm-vim
+      vim-pony
+      nim-vim
+      vim-elixir
+      alchemist-vim
+      vim-ocaml
+      purescript-vim
+      haskell-vim
+      dhall-vim
+      vim-toml
+      vim-pug
+      hexmode
     ];
   };
 }
---
+++ vim-config/init.lua
@@ -0,0 +1,219 @@
+local cmd = vim.cmd       -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn         -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g           -- a table to access global variables
+local opt = vim.opt       -- to set options
+
+local function map(mode, lhs, rhs, opts)
+  local options = { silent = true, noremap = true }
+  if opts then options = vim.tbl_extend('force', options, opts) end
+  vim.keymap.set(mode, lhs, rhs, options)
+end
+
+-- general options
+cmd 'colorscheme molokai'
+opt.syntax = 'on'
+opt.filetype = 'on'
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 0
+opt.shiftwidth = 2
+opt.smarttab = true
+opt.autoindent = true
+opt.smartindent = true
+opt.smartcase = true
+opt.ignorecase = true
+opt.modeline = true
+opt.compatible = false
+opt.encoding = 'utf-8'
+opt.incsearch = true
+opt.hlsearch = true
+opt.history=700
+opt.number = true
+opt.laststatus = 2
+opt.signcolumn = 'yes'
+
+opt.termguicolors = true
+opt.background = 'dark'
+g.airline_theme = 'molokai'
+
+-- ripgrep
+opt.grepprg = 'rg --'
+map('n', 'silent> F', ':Rg<CR>')
+
+-- ctrlp
+g.ctrlp_user_command = 'fd -H --type f --color=never "" %s'
+g.ctrlp_use_caching = 0
+
+g['fruzzy#usenative'] = 1
+g.ctrlp_match_func = {match = 'fruzzy#ctrlp#matcher'}
+
+-- use CTRL+Space to autocomplete
+map('i', '<C-Space>', '<C-x><C-o>')
+map('i', '<C-@>', '<C-Space>')
+
+g.delimitMate_expand_cr = 1
+g.delimitMate_expand_space = 1
+
+-- Enable integration with airline.
+g['airline#extensions#ale#enabled'] = 1
+
+-- Error and warning signs.
+g.ale_sign_error = '⤫'
+g.ale_sign_warning = '⚠'
+
+-- disbale deno as a linter
+g.ale_linters_ignore = {
+  typescript = {'deno'},
+  typescriptreact = {'deno'},
+  nix = {'nix', 'statix'},
+}
+
+g.rooter_patterns = {'package.json', '.git', '.git/', '_darcs/', '.hg/', '.bzr/', '.svn/'}
+
+-- vim-go
+g.go_highlight_build_constraints = 1
+g.go_highlight_extra_types = 1
+g.go_highlight_fields = 1
+g.go_highlight_functions = 1
+g.go_highlight_methods = 1
+g.go_highlight_operators = 1
+g.go_highlight_structs = 1
+g.go_highlight_types = 1
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "go",
+  callback = function()
+    map('n', '<leader>b', '<Plug>(go-build)')
+    map('n', '<leader>r', '<Plug>(go-run)')
+    map('n', '<leader>t', '<Plug>(go-test)')
+    map('n', '<leader>c', '<Plug>(go-coverage-toggle)')
+  end,
+})
+
+-- markdown
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    vim.opt_local.spell = true
+  end,
+})
+
+-- LSP
+map('n', 'E', vim.diagnostic.open_float)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+end
+
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local lspconfig = require('lspconfig')
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+  },
+}
+
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+require('lspconfig')['nimls'].setup{
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = lsp_flags,
+}
+require('lspconfig')['ocamllsp'].setup{
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = lsp_flags,
+}
+require('lspconfig')['pyright'].setup{
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = lsp_flags,
+}
+require('lspconfig')['tsserver'].setup{
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = lsp_flags,
+    cmd = { 'pnpm', 'dlx', 'typescript-language-server', '--stdio'  },
+}
+require('lspconfig')['zls'].setup{
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = lsp_flags,
+}
+require('lspconfig')['rust_analyzer'].setup{
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = lsp_flags,
+    -- Server-specific settings...
+    settings = {
+      ["rust-analyzer"] = {}
+    }
+}
+require('lspconfig')['dockerls'].setup{
+    on_attach = on_attach,
+    capabilities = capabilities,
+    flags = lsp_flags,
+    cmd = { 'pnpm', 'dlx', "dockerfile-language-server-nodejs", "docker-langserver", "--stdio" }
+}
+
